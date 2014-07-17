24fd0ad9bc02ac063f973f39f57983e35989d674
thanushcst <thanushcst@gmail.com>
2014-07-17 23:17:50 +0530

    Loaded the template

--- www/js/dSurvey.js
+++ www/js/dSurvey.js
@@ -1,8 +1,14 @@
-define(function() {
+define(['handlebars', 'requireText!views/home.hbts'],function(handlebar,home) {
     var container;
 
     var dSurvey = function(containerId) {
-        console.log(dSurvey);
+        this.container = document.getElementById(containerId);
+    };
+
+    dSurvey.prototype.init = function() {
+
+        //this.home = handlebar.default.compile(home);
+        this.container.innerHTML = home;
     };
 
     return dSurvey;
