9b423c39af61a6f78e184a169e86b122a878cc2e
Kanagarajah Thanushan <thanushan@info-share.org>
2014-07-24 12:50:37 +0530

    Clear the code

--- www/data/metaData.js
+++ www/data/metaData.js
@@ -2,7 +2,11 @@ define(function(){
     return {
         configData: {
             title: "D-Survey",
-            description: "Digital Survey Management System"
+            description: "Digital Survey Management System",
+            footer: {
+                copyright: 'Copyright © 2014',
+                version: '0.0.1'
+            }
         }
     };
 });
